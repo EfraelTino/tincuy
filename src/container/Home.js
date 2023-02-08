a122a5e28c6104a9116440d2c32a70cfbdd7d3a6
efrael <efrael2001@gmail.com>
2023-02-08 20:42:13 +0100

    firebase login

--- src/container/Home.js
+++ src/container/Home.js
@@ -1,4 +1,4 @@
-import React,{useState ,useEffect} from 'react';
+import React from 'react';
 import Featured from '../components/HomeComponent/Featured';
 import Miles from '../components/HomeComponent/Miles';
 import Partners from '../components/HomeComponent/Partners';
@@ -7,17 +7,7 @@ import Story from '../components/HomeComponent/Story';
 import useObserver from '../hooks/useObserver';
 
 const Home = () => {
-    const [scrollY, setScrollY] = useState(0);
 
-    const [observer, setElements, entries] = useObserver({
-        threshold:0.25,
-        root:null,
-    })
-
-    useEffect(() =>{
-        const datos = document.querySelector('.featured');
-        setElements(datos)
-    }, [setElements])
     return (
        <>
        <PrincipalComponent/>
